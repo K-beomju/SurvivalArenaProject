2ff6bb5c7e95a86d540cdbd23ed5740e0102cb37
Kimbeomju <kbj52264291@gmail.com>
2023-01-31 22:34:31 +0900

    게임한사이클 안정적으로 돌게 구현 , 모노싱글톤으로 교체

--- Survival_Game/Assets/01.Scripts/Managers/PoolManager.cs
+++ Survival_Game/Assets/01.Scripts/Managers/PoolManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 
-public class PoolManager : MonoBehaviour
+public class PoolManager : Singleton<PoolManager>
 {
     #region ObjectPrefabs
     public GameObject arrowPrefab;
@@ -17,35 +17,8 @@ public class PoolManager : MonoBehaviour
 
     private Dictionary<string, int> effectDic = new Dictionary<string, int>();
 
-    private static PoolManager Instance;
-    public static PoolManager instance
-    {
-        get
-        {
-            if (Instance == null)
-            {
-                Instance = FindObjectOfType(typeof(PoolManager)) as PoolManager;
-                if (Instance == null)
-                {
-                    GameObject obj = new GameObject();
-                    Instance = obj.AddComponent<PoolManager>();
-                }
-            }
-
-            return Instance;
-        }
-    }
-
     void Awake()
     {
-        if (Instance == null)
-        {
-            Instance = this;
-            DontDestroyOnLoad(this.gameObject);
-        }
-        else
-            Destroy(this.gameObject);
-
         arrowPool = new ObjectPooling<Arrow>(arrowPrefab, this.transform, 10);
         enemyPool = new ObjectPooling<Enemy>(enemyPrefab, this.transform , 10);
 
@@ -53,11 +26,11 @@ public class PoolManager : MonoBehaviour
 
     public static Arrow GetArrowObject()
     {
-        return instance.arrowPool.GetOrCreate();
+        return Instance.arrowPool.GetOrCreate();
     }
 
     public static Enemy GetEnemyObject()
     {
-        return instance.enemyPool.GetOrCreate();
+        return Instance.enemyPool.GetOrCreate();
     }
 }
